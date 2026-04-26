e4a294332a8a9b88d136fbb556185c4e7538f4be
Almas <aalmasabilov@gmail.com>
2026-04-26 23:55:57 +0500

    Implement slow motion time state

---
+++ core/src/main/java/com/chronorift/game/time/SlowMotionState.java
@@ -0,0 +1,14 @@
+package com.chronorift.game.time;
+
+public class SlowMotionState extends AbstractTimeState{
+
+    public SlowMotionState(){
+        super(
+            "Slow Motion Zone",
+            0.45f,
+            0.85f,
+            0.60f,
+            1.30f
+        );
+    }
+}
