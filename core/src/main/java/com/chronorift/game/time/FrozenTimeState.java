44ab55e308f184ef463a53ad9e0332bf0061111c
Almas <aalmasabilov@gmail.com>
2026-04-27 00:00:03 +0500

    Implement frozen time state

---
+++ core/src/main/java/com/chronorift/game/time/FrozenTimeState.java
@@ -0,0 +1,14 @@
+package com.chronorift.game.time;
+
+public class FrozenTimeState extends AbstractTimeState{
+
+    public FrozenTimeState(){
+        super(
+            "Frozen Timeline",
+            0.35f,
+            0.75f,
+            0.50f,
+            1.40f
+        );
+    }
+}
