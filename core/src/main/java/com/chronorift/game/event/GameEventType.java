7b1f096b9b6ee4758e29d013fd04fd2683759a33
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-17 15:43:47 +0500

    Prepare gameplay event type list

---
+++ core/src/main/java/com/chronorift/game/event/GameEventType.java
@@ -0,0 +1,11 @@
+package com.chronorift.game.events.event;
+
+public enum GameEventType {
+    LOG,
+    TIME_STATE_CHANGED,
+    LEVEL_CHANGED,
+    ENEMY_KILLED,
+    BOSS_SPAWNED,
+    PLAYER_DAMAGED,
+    PLAYER_REWOUND
+}
