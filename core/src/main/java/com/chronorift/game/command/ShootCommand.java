0a7c57c67c5cb5454c956cef0bce599d769eab07
SaoMadao <zhassulan.estai@narxoz.kz>
2026-05-18 13:10:42 +0500

    Handle shooting command execution

--- core/src/main/java/com/chronorift/game/command/ShootCommand.java
+++ core/src/main/java/com/chronorift/game/command/ShootCommand.java
@@ -1,4 +1,17 @@
 package com.chronorift.game.command;
 
-public class ShootCommand {
+import com.badlogic.gdx.math.Vector2;
+import com.chronorift.game.world.GameWorld;
+
+public class ShootCommand implements GameCommand {
+    private final Vector2 target;
+
+    public ShootCommand(Vector2 target) {
+        this.target = target;
+    }
+
+    @Override
+    public void execute(GameWorld world, float delta) {
+        world.playerShoot(target);
+    }
 }
