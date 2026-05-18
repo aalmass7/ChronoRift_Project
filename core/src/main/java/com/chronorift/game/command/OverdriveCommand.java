3f1eeacb0dd33d6f9eeda73f0a2c17c7fa5a1b61
SaoMadao <zhassulan.estai@narxoz.kz>
2026-05-18 13:12:08 +0500

    Wire overdrive activation command

--- core/src/main/java/com/chronorift/game/command/OverdriveCommand.java
+++ core/src/main/java/com/chronorift/game/command/OverdriveCommand.java
@@ -1,4 +1,10 @@
 package com.chronorift.game.command;
 
-public class OverdriveCommand {
+import com.chronorift.game.world.GameWorld;
+
+public class OverdriveCommand implements GameCommand {
+    @Override
+    public void execute(GameWorld world, float delta) {
+        world.activateOverdrive();
+    }
 }
