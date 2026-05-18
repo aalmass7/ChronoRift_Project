561b1ec6ccb63da86b3e496eef36a884a21cb3ce
SaoMadao <zhassulan.estai@narxoz.kz>
2026-05-18 23:51:48 +0500

    Define chrono tool types

--- core/src/main/java/com/chronorift/game/tool/ToolType.java
+++ core/src/main/java/com/chronorift/game/tool/ToolType.java
@@ -1,4 +1,10 @@
 package com.chronorift.game.tool;
 
-public class ToolType {
+public enum ToolType {
+    CHRONO_SHIELD,
+    CHRONO_ELIXIR,
+    TIME_STABILIZER,
+    ANTI_FREEZE,
+    PHASE_CRYSTAL,
+    REWIND
 }
