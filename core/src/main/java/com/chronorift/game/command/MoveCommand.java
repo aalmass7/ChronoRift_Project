1b5abd509a6a1366a3a12227f574b319016560b8
SaoMadao <zhassulan.estai@narxoz.kz>
2026-05-18 13:10:15 +0500

    Handle movement command execution

--- core/src/main/java/com/chronorift/game/command/MoveCommand.java
+++ core/src/main/java/com/chronorift/game/command/MoveCommand.java
@@ -1,4 +1,17 @@
 package com.chronorift.game.command;
 
-public class MoveCommand {
+import com.badlogic.gdx.math.Vector2;
+import com.chronorift.game.world.GameWorld;
+
+public class MoveCommand implements GameCommand {
+    private final Vector2 direction;
+
+    public MoveCommand(Vector2 direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public void execute(GameWorld world, float delta) {
+        world.movePlayer(direction);
+    }
 }
