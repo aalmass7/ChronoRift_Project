dc82e5132a51b385ca3367e0033bb14706ed86fe
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-18 11:56:18 +0500

    Add shared game configuration constants

---
+++ core/src/main/java/com/chronorift/game/core/GameConfig.java
@@ -0,0 +1,14 @@
+package com.chronorift.game.core;
+
+public final class GameConfig {
+    public static final float WORLD_WIDTH = 1280f;
+    public static final float WORLD_HEIGHT = 720f;
+    public static final float PLAYER_RADIUS = 28f;
+    public static final float ENEMY_RADIUS = 26f;
+    public static final float BOSS_RADIUS = 46f;
+    public static final float PROJECTILE_RADIUS = 8f;
+    public static final int MAX_LOG_LINES = 6;
+
+    private GameConfig() {
+    }
+}
