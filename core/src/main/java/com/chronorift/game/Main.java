4f0f8f17b32f1892b7f5d6b25f585f7275719ce4
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-05-20 19:32:29 +0500

    Finalize desktop launch flow

--- core/src/main/java/com/chronorift/game/Main.java
+++ core/src/main/java/com/chronorift/game/Main.java
@@ -1,32 +1,56 @@
 package com.chronorift.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.chronorift.game.core.GameAssets;
+import com.chronorift.game.screen.EndScreen;
+import com.chronorift.game.screen.GameScreen;
+import com.chronorift.game.screen.MenuScreen;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
     private SpriteBatch batch;
-    private Texture image;
+    private BitmapFont font;
+    private GameAssets assets;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        font = new BitmapFont();
+        font.getData().setScale(1.1f);
+        assets = new GameAssets();
+        assets.load();
+        setScreen(new MenuScreen(this));
     }
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+    public void startGame() {
+        setScreen(new GameScreen(this));
+    }
+
+    public void showEndScreen(boolean victory, int score) {
+        setScreen(new EndScreen(this, victory, score));
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public BitmapFont getFont() {
+        return font;
+    }
+
+    public GameAssets getAssets() {
+        return assets;
     }
 
     @Override
     public void dispose() {
+        super.dispose();
+        if (screen != null) {
+            screen.dispose();
+        }
+        assets.dispose();
+        font.dispose();
         batch.dispose();
-        image.dispose();
     }
 }
